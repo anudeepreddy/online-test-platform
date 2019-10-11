e1bce7a5980ae57e430f62811e45b85fa01fece3
anudeepreddy <anudeepc85@gmail.com>
2019-10-11 23:00:08 +0530

    Added stores for exam

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -15,7 +15,7 @@
                 <Topnav />
             <div class="container-fluid">
                 <div class="text-center mt-5">
-                    <div class="error mx-auto" data-text="404">
+                    <div class="error mx-auto" data-text={status}>
                         <p class="m-0">{status}</p>
                     </div>
                     <p class="text-black-50 mb-0">It looks like you found a glitch in the matrix...</p><a href="/">← Back to Dashboard</a></div>
--- src/routes/exam.svelte
+++ src/routes/exam.svelte
@@ -1,23 +1,43 @@
 <script>
 import Topnav from '../components/Topnav.svelte'
 import Footer from '../components/Footer.svelte'
-var countDownDate = new Date("Oct 9, 2019 22:37:25").getTime();
+import {questions,answers} from '../stores/exam.js'
+var countDownDate = new Date("Oct 31, 2019 22:37:25").getTime();
+var time = "0:00:00";
+var Active = {
+    no: $questions[0].no,
+    question: $questions[0].data
+};
+function printAnswers(){
+    console.log($answers);
+}
+var len = $questions.length;
+console.log($questions[0].no);
+function changeActive(qN){
+    for(let i=0;i<len;i++){
+        if($questions[i].no==qN){
+            qN=i;
+            break;
+        }
+    }
+    Active.no = $questions[qN].no;
+    Active.question = $questions[qN].data;
+}
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("timer").innerHTML = hours + "h "
+  time = hours + "h "
   + minutes + "m " + seconds + "s "; 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "Test Ended";
+    time = "Test Ended";
   }
 }, 1000);
 </script>
+
 <body id="page-top">
     <div id="wrapper">
         <div class="d-flex flex-column" id="content-wrapper">
@@ -29,24 +49,41 @@ var x = setInterval(function() {
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title">Timer</h4>
-                            <h6 class="text-muted card-subtitle mb-2" id="timer">00:00:00</h6>
+                            <h6 class="text-muted card-subtitle mb-2" id="timer">{time}</h6>
                         </div>
                     </div>
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Question 1</h4>
-                            <p class="card-text">This is a sample Question?</p>
-                            <div class="form-check" id="option1"><input class="form-check-input" type="radio" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Label</label></div>
-                            <div class="form-check" id="option2"><input class="form-check-input" type="radio" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Label</label></div>
-                            <div class="form-check" id="option3"><input class="form-check-input" type="radio" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Label</label></div>
-                            <div class="form-check" id="option4"><input class="form-check-input" type="radio" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Label</label></div>
-                            <div class="btn-group" role="group"><button class="btn btn-primary" type="button">Mark for Review</button></div>
+                            <h4 class="card-title">Question {Active.no}</h4>
+                            <p class="card-text">{Active.question}</p>
+                            <div class="form-check" id="option1">
+                                <input class="form-check-input" type="radio" id="formCheck-1" bind:group={$answers[Active.no]} value={1}>
+                                <label class="form-check-label" for="formCheck-1">A</label>
+                            </div>
+                            <div class="form-check" id="option2">
+                                <input class="form-check-input" type="radio" id="formCheck-2" bind:group={$answers[Active.no]} value={2}>
+                                <label class="form-check-label" for="formCheck-1">B</label>
+                            </div>
+                            <div class="form-check" id="option3">
+                                <input class="form-check-input" type="radio" id="formCheck-3" bind:group={$answers[Active.no]} value={3}>
+                                <label class="form-check-label" for="formCheck-1">C</label>
+                            </div>
+                            <div class="form-check" id="option4">
+                                <input class="form-check-input" type="radio" id="formCheck-4" bind:group={$answers[Active.no]} value={4}>
+                                <label class="form-check-label" for="formCheck-1">D</label>
+                            </div>
+                            <div class="btn-group" role="group">
+                                <button class="btn btn-primary" type="button">Mark for Review</button>
+                                <button class="btn btn-primary" type="button" on:click={printAnswers}>Debug</button>
+                            </div>
                         </div>
                     </div>
                     <div class="card" id="questions-list">
-                        <div class="card-body"><button class="btn btn-primary" type="button" style="margin-right:16px">1</button><button class="btn btn-primary" type="button" style="margin-right:16px">2</button><button class="btn btn-primary" type="button" style="margin-right:16px">3</button>
-                            <button
-                                class="btn btn-primary" type="button" style="margin-right:16px">4</button><button class="btn btn-primary" type="button" style="margin-right:16px">5</button></div>
+                        <div class="card-body">
+                        {#each $questions as question}
+                        <button class="btn btn-primary" type="button" style="margin-right:16px" on:click={()=>changeActive(question.no)}>{question.no}</button>
+                        {/each}
+                        </div>
                     </div>
                 </div>
             </div>

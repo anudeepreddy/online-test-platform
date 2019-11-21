780af91723a416bad75f2b4f7518aa9d52200418
anudeepreddy <anudeepc85@gmail.com>
2019-11-21 09:07:02 +0530

    added mathtype

---
+++ src/routes/admin/create_question.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Sidebar from "../../components/Sidebar";
+    import Topnav from "../../components/Topnav";
+    import Footer from "../../components/Footer";
+    import { onMount } from 'svelte';
+    onMount(()=>{CKEDITOR.replace( 'editor1' );});
+</script>
+
+<svelte:head>
+
+  <title>Create Question</title>
+</svelte:head>
+<body id="page-top">
+  <div id="wrapper">
+    <Sidebar />
+    <div class="d-flex flex-column" id="content-wrapper">
+      <div id="content">
+        <Topnav />
+       <form>
+            <textarea name="editor1" id="editor1" rows="10" cols="80">
+                Type your question here include options as well
+            </textarea>
+        </form>
+        
+      </div>
+      <Footer />
+    </div>
+    <a class="border rounded d-inline scroll-to-top" href="#page-top">
+      <i class="fas fa-angle-up" />
+    </a>
+  </div>
+</body>
